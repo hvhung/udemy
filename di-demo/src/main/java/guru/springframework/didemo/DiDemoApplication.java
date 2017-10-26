b86d31152cf3293397e27c302977a4e74d5409fe
huynhvanhung <hunghuynhvan@live.com>
2017-10-27 00:05:40 +0700

    Add Spring Environment Properties
    Add multiple property files
    
    next commit will be: refactoring these property files to Spring Boot
    property - application.properties

--- di-demo/src/main/java/guru/springframework/didemo/DiDemoApplication.java
+++ di-demo/src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -3,12 +3,13 @@ package guru.springframework.didemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 import guru.springframework.didemo.controllers.ConstructorInjectedController;
 import guru.springframework.didemo.controllers.GetterInjectedController;
 import guru.springframework.didemo.controllers.MyController;
 import guru.springframework.didemo.controllers.PropertyInjectedController;
+import guru.springframework.didemo.examplebeans.FakeDataSource;
+import guru.springframework.didemo.examplebeans.FakeJmsBroker;
 
 @SpringBootApplication
 //@ComponentScan(basePackages= {"guru.springframework.didemo.controllers", "guru.springframework.didemo.services" })
@@ -16,12 +17,25 @@ public class DiDemoApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
-		MyController controller = (MyController) ctx.getBean("myController");
-		System.out.println("----- " + controller.hello());
 		
-		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+      MyController controller = (MyController) ctx.getBean("myController");
+		
+      //exampleBean(ctx, controller);
+      addMultiplePropertyFiles(ctx, controller);
+	}
+	
+	static void addMultiplePropertyFiles(ApplicationContext ctx, MyController controller) {
+	   FakeDataSource dataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+	   System.out.println(dataSource.getUser());
+	   
+	   FakeJmsBroker jms = (FakeJmsBroker) ctx.getBean(FakeJmsBroker.class);
+	   System.out.println(jms.getUsername());
+	}
+	
+	static void exampleBean(ApplicationContext ctx, MyController controller) {
+      System.out.println("----- " + controller.hello());
+	   System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
       System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
       System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
 	}
-	
 }
--- di-demo/src/main/java/guru/springframework/didemo/config/PropertyConfig.java
+++ di-demo/src/main/java/guru/springframework/didemo/config/PropertyConfig.java
@@ -1,16 +1,27 @@
 package guru.springframework.didemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 import guru.springframework.didemo.examplebeans.FakeDataSource;
+import guru.springframework.didemo.examplebeans.FakeJmsBroker;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//@PropertySource("classpath:datasource.properties")
+@PropertySources({
+   @PropertySource("classpath:datasource.properties"),
+   @PropertySource("classpath:jms.properties")
+})
 public class PropertyConfig {
+
+   @Autowired
+   Environment env;
 
    @Value("${guru.username}")
    String user;
@@ -21,13 +32,31 @@ public class PropertyConfig {
    @Value("${guru.dburl}")
    String url;
 
+   @Value("${guru.jms.username}")
+   String jmsUsername;
+
+   @Value("${guru.jms.password}")
+   String jmsPassoword;
+
+   @Value("${guru.jms.url}")
+   String jmsUrl;
+
    @Bean
    public FakeDataSource fakeDataSource() {
       FakeDataSource fakeDataSource = new FakeDataSource();
-      fakeDataSource.setUser(user);
+      fakeDataSource.setUser(env.getProperty("USERNAME"));
       fakeDataSource.setPassword(password);
       fakeDataSource.setUrl(url);
       return fakeDataSource;
+   }
+
+   @Bean
+   public FakeJmsBroker fakeJmsBroker() {
+      FakeJmsBroker jmsBroker = new FakeJmsBroker();
+      jmsBroker.setUsername(jmsUsername);
+      jmsBroker.setPassword(jmsPassoword);
+      jmsBroker.setUrl(jmsUrl);
+      return jmsBroker;
    }
 
    @Bean
