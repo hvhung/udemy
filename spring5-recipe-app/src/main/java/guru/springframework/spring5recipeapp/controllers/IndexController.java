13423acfd678dde01ee1b0642650fcaa461d5ad8
huynhvanhung <hunghuynhvan@live.com>
2017-10-27 23:44:44 +0700

    add Repositories

--- spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
+++ spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,13 +1,34 @@
 package guru.springframework.spring5recipeapp.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.spring5recipeapp.model.Category;
+import guru.springframework.spring5recipeapp.model.UnitOfMeasure;
+import guru.springframework.spring5recipeapp.repositories.CategoryRepository;
+import guru.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
 
 @Controller
 public class IndexController {
 
+   private CategoryRepository categoryRepository;
+   private UnitOfMeasureRepository unitOfMeasureRepository;
+   
+   public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+      this.categoryRepository = categoryRepository;
+      this.unitOfMeasureRepository = unitOfMeasureRepository;
+   }
+
    @RequestMapping({"","/","/index"})
    public String getIndexPage() {
+      Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+      Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+      
+      System.out.println("Cat Id is: " + categoryOptional.get().getId());
+      System.out.println("UOM ID is: " + unitOfMeasureOptional.get().getId());
+      
       return "index";
    }
    
---
+++ spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring5recipeapp.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import guru.springframework.spring5recipeapp.model.Category;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+   Optional<Category> findByDescription(String description);
+}
